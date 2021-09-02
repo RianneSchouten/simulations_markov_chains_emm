0215085ddbf9acd0473db2d5b521c575147aadf2
RianneSchouten <rianneschouten@live.nl>
2021-09-02 15:28:17 +0200

    poster figures

---
+++ figures/Figures_poster.R
@@ -0,0 +1,188 @@
+library(ggplot2)
+library(readxl)
+library(tidyverse)
+library(RColorBrewer)
+library(mice)
+library(xtable)
+
+setwd("C:/Users/20200059/Documents/Github/simulations_beam_search_markov_chain/figures/")
+
+#### Simulation results and figures for revised manuscript May 2021 ####
+
+data_phiwd <- read_excel("../data_output/results_revised_manuscript/experiment_higherorders_phiwd_20210518_[50, [100], [20, 10, 5], [200, 50, 10], [10, 5, 2], [0.5], [2], [1]].xlsx")
+data_rest <- read_excel("../data_output/results_manuscript/experiment_higherorders_20210107_10nreps_[100]_[200, 50, 10]_[10, 5, 2]_[20, 10, 5].xlsx")
+data_omegatv_phiwrl <- read_excel("../data_output/results_manuscript/experiment_higherorders_20210128_30nreps_[100]_[200, 50, 10]_[10, 5, 2]_[20, 10, 5].xlsx")
+
+data_09 <- read_excel("../data_output/results_manuscript/experiment_higherorders_20210109_10nreps_[100]_[200, 50, 10]_[10, 5, 2]_[20, 10, 5].xlsx")
+data_10 <- read_excel("../data_output/results_manuscript/experiment_higherorders_20210110_10nreps_[100]_[200, 50, 10]_[10, 5, 2]_[20, 10, 5].xlsx")
+data_11 <- read_excel("../data_output/results_manuscript/experiment_higherorders_20210115_10nreps_[100]_[200, 50, 10]_[10, 5, 2]_[20, 10, 5].xlsx")
+data_13 <- read_excel("../data_output/results_revised_manuscript/experiment_higherorders_20210513_[10, [100], [20, 10, 5], [200, 50, 10], [10, 5, 2], [0.5], [2], [1]].xlsx")
+
+total_reps = 10 + 10 + 10 + 10 + 10
+
+length_new_column = 1 * 4 * 50 * 1 * 3 * 3 * 3
+long_data_phiwd <- data_phiwd %>%
+  pivot_longer(
+    cols = names(data_phiwd)[12:dim(data_phiwd)[2]],
+    names_to = "measure",
+    values_to = "value") %>%
+  add_column(type = rep(c('rank', 'order', 'found_order'), length_new_column)) %>%
+  mutate(measure = gsub("_.*", "", measure)) %>%
+  select(-c(true_desc_length, global_model_order, start_at_order, p))
+
+length_new_column = 2 * 4 * 30 * 1 * 3 * 3 * 3
+long_data_phiwrl_omegatv <- data_omegatv_phiwrl %>%
+  pivot_longer(
+    cols = names(data_omegatv_phiwrl)[9:dim(data_omegatv_phiwrl)[2]],
+    names_to = "measure",
+    values_to = "value") %>%
+  add_column(type = rep(c('rank', 'order', 'found_order'), length_new_column)) %>%
+  mutate(measure = gsub("_.*", "", measure)) %>%
+  select(-c(refs))
+
+length_new_column = 3 * 4 * 10 * 1 * 3 * 3 * 3
+long_data_rest <- data_rest %>%
+  pivot_longer(
+    cols = names(data_rest)[9:dim(data_rest)[2]],
+    names_to = "measure",
+    values_to = "value") %>%
+  add_column(type = rep(c('rank', 'order'), length_new_column)) %>%
+  mutate(measure = gsub("_.*", "", measure)) %>%
+  select(-c(refs))
+
+length_new_column = 6 * 4 * 10 * 1 * 3 * 3 * 3
+long_data_09 <- data_09 %>%
+  pivot_longer(
+    cols = names(data_09)[9:dim(data_09)[2]],
+    names_to = "measure",
+    values_to = "value") %>%
+  add_column(type = rep(c('rank', 'order'), length_new_column)) %>%
+  mutate(measure = gsub("_.*", "", measure)) %>%
+  select(-c(refs)) %>% filter(measure != 'phiwd') %>%
+  filter(measure != 'phiwrl') %>% filter(measure != 'omegatv')
+
+long_data_10 <- data_10 %>%
+  pivot_longer(
+    cols = names(data_10)[9:dim(data_10)[2]],
+    names_to = "measure",
+    values_to = "value") %>%
+  add_column(type = rep(c('rank', 'order'), length_new_column)) %>%
+  mutate(measure = gsub("_.*", "", measure)) %>%
+  select(-c(refs)) %>% filter(measure != 'phiwd') %>%
+  filter(measure != 'phiwrl') %>% filter(measure != 'omegatv')
+
+length_new_column = 6 * 4 * 10 * 1 * 3 * 3 * 3
+long_data_11 <- data_11 %>%
+  pivot_longer(
+    cols = names(data_11)[9:dim(data_11)[2]],
+    names_to = "measure",
+    values_to = "value") %>%
+  add_column(type = rep(c('rank', 'order', 'found_order'), length_new_column)) %>%
+  mutate(measure = gsub("_.*", "", measure)) %>%
+  select(-c(refs)) %>% filter(measure != 'phiwd')
+
+length_new_column = 6 * 4 * 10 * 1 * 3 * 3 * 3
+long_data_13 <- data_13 %>%
+  pivot_longer(
+    cols = names(data_13)[12:dim(data_13)[2]],
+    names_to = "measure",
+    values_to = "value") %>%
+  add_column(type = rep(c('rank', 'order', 'found_order'), length_new_column)) %>%
+  mutate(measure = gsub("_.*", "", measure)) %>%
+  filter(measure != 'phiwd') %>%
+  select(-c(true_desc_length, global_model_order, start_at_order, p))
+
+long_data <- rbind(long_data_phiwd, long_data_rest,
+                   long_data_phiwrl_omegatv,
+                   long_data_09, long_data_10, 
+                   long_data_11, long_data_13) %>%
+  arrange(nreps, as.integer(N), as.integer(T), as.integer(S), 
+          as.integer(ncovs), as.integer(subgroup_orders)) %>%
+  mutate(true_subgroup_order = as.factor(subgroup_orders)) %>%
+  mutate(states = ordered(factor(S), 
+                          levels = as.character(sort(as.integer(unique(S)))))) %>%
+  mutate(timepoints = ordered(factor(T), 
+                              levels = as.character(sort(as.integer(unique(T)))))) %>%
+  mutate(ncovs = ordered(factor(ncovs), 
+                         levels = as.character(sort(as.integer(unique(ncovs)))))) %>%
+  mutate(N = ordered(factor(N), 
+                     levels = as.character(sort(as.integer(unique(N)))))) %>%
+  mutate(measure = ordered(factor(measure), 
+                           levels = c('phibic', 'phiaic', 'phiaicc', 'phiwd', 'omegatv', 'phiwrl')))
+
+# two figures
+plot_data <- long_data %>%
+  filter(N == 100) %>%
+  filter(ncovs == 20)
+
+plot_data_2 <- plot_data %>%
+  filter(type == 'rank') %>%
+  filter(measure == 'phiaic') %>%
+  group_by(true_subgroup_order, timepoints, states) %>%
+  summarize(M = median(value))
+
+ranks <- plot_data %>% 
+  filter(type == 'rank') %>%
+  filter(measure == 'phiaic') %>%
+  #filter(timepoints == 50) %>%
+  filter(states == 5) %>%
+  #filter(true_subgroup_order == 3) %>%
+  #mutate(measure = recode(measure, phiaic = "phibic, phiaic, phiaicc")) %>%
+  ggplot(aes(y = value, x = true_subgroup_order)) +
+  geom_boxplot(aes(fill = timepoints), outlier.shape = NA) + 
+  #facet_grid(measure ~ timepoints, labeller = label_both) + 
+  labs(title = paste('Boxplots of the rank of the true subgroup in the top-20', 
+                     sep = " ", collapse = NULL),
+       fill = 'states') + 
+  xlab('True subgroup order') + 
+  ylab('Rank') +
+  guides(fill = guide_legend(direction = "horizontal")) +
+  theme(legend.position="top",
+        legend.justification="right",
+        plot.title = element_text(vjust=-4), 
+        legend.box.margin = margin(-1,0,0,0, "line"),
+        #axis.title.y = element_text(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank()) #+ 
+  #scale_fill_manual(values=c("#deebf7", "#9ecae1", "#3182bd"))
+
+ranks
+
+plot_data <- long_data %>%
+  filter(N == 100) %>%
+  filter(ncovs == 20) %>%
+  filter(type == 'order') %>%
+  group_by(N, measure, true_subgroup_order, timepoints, states, ncovs) %>%
+  summarize(perc_true_order = 
+              100 * sum(subgroup_orders == 
+                          value, na.rm = TRUE) / total_reps)
+
+orders <- plot_data %>%
+  filter(measure == 'phiaic') %>%
+  filter(timepoints == 200) %>%
+  #filter(states == 5) %>%
+  ggplot(aes(x = true_subgroup_order, y = perc_true_order, 
+             fill = states)) + 
+  geom_bar(stat = "identity", position = position_dodge(), colour="black") + 
+  #facet_grid(measure ~ timepoints, labeller = label_both) + 
+  labs(title = paste('Percentage of nreps where the true subgroup order is found', 
+                     sep = " ", collapse = NULL)) +
+  xlab('True subgroup order') + 
+  ylab('Percentage') +
+  guides(fill = guide_legend(direction = "horizontal")) +
+  theme(legend.position="top",
+        legend.justification="right",
+        plot.title = element_text(vjust=-4), 
+        legend.box.margin = margin(-1,0,0,0, "line"),
+        #axis.title.y = element_text(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank()) 
+#+ 
+ # scale_fill_manual(values=c("#deebf7", "#9ecae1", "#3182bd")) 
+
+orders
+
